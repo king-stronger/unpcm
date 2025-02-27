6bf964f73b1825cf656c2e915286d4193ed63425
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-02-27 14:59:39 +0000

    Rectify the page attribute because the index file wasn't accessible

--- router/routes.js
+++ router/routes.js
@@ -4,8 +4,8 @@ const router = Router();
 
 router.get("/", (req, res, next) => {
     res.render("layout", { 
-        page: "UNCMP - Accueil",
-        pageTitle: "UNCMP"
+        page: "index",
+        pageTitle: "UNCMP - Accueil"
     });
 });
 
