e3687474f31c1314ef864e4798c829a5e76b1504
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-03-21 14:51:07 +0000

    Style the faq question and make it work, add some colors on different parts of the website, remove the content-ratio

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -18,3 +18,21 @@ function addAnimation(){
         });
     });
 }
+
+const toggles = document.querySelectorAll(".faq-plus");
+
+toggles.forEach(icon => {
+    icon.addEventListener("click", () => {
+        const faqItem = icon.closest(".faq-item");
+
+        faqItem.classList.toggle("show");
+
+        if (icon.classList.contains("bx-plus")) {
+            icon.classList.remove("bx-plus");
+            icon.classList.add("bx-minus");
+        } else {
+            icon.classList.remove("bx-minus");
+            icon.classList.add("bx-plus");
+        }
+    });
+});
