755224d1bcd88e68d20f97ae7f4e3943071bd498
king-stronger <114509657+king-stronger@users.noreply.github.com>
2025-04-30 12:34:56 +0000

    Reset the project

--- middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    let status = err.status || 500;
-    res.render("error", {
-        message: err.message,
-        status
-    })
-}
-
-export default errorHandler;
