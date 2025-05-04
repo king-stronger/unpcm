cdb849d42454b70290efb0116b38f6388423c334
king-stronger <114509657+king-stronger@users.noreply.github.com>
2025-05-04 11:39:26 +0000

    remove: the scroll script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,4 @@
 const menu = document.querySelector("#nav-list")
-const scrollers = document.querySelectorAll(".scroller");
-
-if(!window.matchMedia("(prefers-reduce-motion: reduce)").matches){
-    addAnimation();
-}
-
-function addAnimation(){
-    scrollers.forEach(scroller => {
-        scroller.setAttribute("data-animated", "true");
-
-        const scrollerInner = scroller.querySelector(".scroller-inner");
-        const scrollerContent = Array.from(scrollerInner.children);
-
-        scrollerContent.forEach(item => {
-            const duplicatedItem = item.cloneNode(true);
-            duplicatedItem.setAttribute("aria-hidden", "true");
-            scrollerInner.appendChild(duplicatedItem);
-        });
-    });
-}
 
 function toggleMenu(){
     if(menu.classList.contains("show")){
